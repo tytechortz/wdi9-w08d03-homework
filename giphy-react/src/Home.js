bdcb113d4e3c9fb1ba5a951f1af245e89693e061
James <tytechortz@gmail.com>
2018-11-14 15:58:03 -0700

    finishes app

--- giphy-react/src/Home.js
+++ giphy-react/src/Home.js
@@ -8,34 +8,9 @@ class Home extends Component {
     super();
 
     this.state = {
-      gifs: [
-        {
-          "type": "gif",
-          "id": "iuHaJ0D7macZq",
-          "url": "http://giphy.com/gifs/cat-day-tomorrow-iuHaJ0D7macZq",
-          "source": "https://www.reddit.com/r/CatGifs/comments/5f0h9a/tomorrow_is_legs_day/",
-          "rating": "pg",
-          "images": {
-            "fixed_height": {
-              "url": "http://media4.giphy.com/media/iuHaJ0D7macZq/200.gif"
-            }
-          }
-        },
-        {
-          "type": "gif",
-          "id": "Z1kpfgtHmpWHS",
-          "url": "http://giphy.com/gifs/cat-way-make-Z1kpfgtHmpWHS",
-          "source": "http://shewhoseeks.blogspot.com/2016/03/cat-gifs-that-make-me-laugh-way-more.html",
-          "rating": "g",
-          "images": {
-            "fixed_height": {
-              "url": "http://media4.giphy.com/media/Z1kpfgtHmpWHS/200.gif"
-            }
-          }
-        }
-      ]
-    }
-  }
+      gifs: []
+  };
+}
 
 handleTermChange = async (term) => {
   const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=2rIWz3Iud2RuGJVSjAYlbPZJ7SEcYaM2`;
